4b7cb4f40598d2b8c5e902faf24697a1807ea0d8
Ekrem <avekremyilmazturk@gmail.com>
2025-04-07 12:26:43 +0300

    test2

--- frontend/src/components/SaleModal.jsx
+++ frontend/src/components/SaleModal.jsx
@@ -10,124 +10,89 @@ import MenuItem from "@mui/material/MenuItem"
 import FormControl from "@mui/material/FormControl"
 import Select from "@mui/material/Select"
 import { useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
 
 export default function SaleModal({ open, handleClose, info, setInfo }) {
+  const navigate = useNavigate()
   const { postStockData, putStockData } = useStockCall()
   const { products, brands } = useSelector((state) => state.stock)
 
   const handleChange = (e) => {
     const { name, value } = e.target
-    // setInfo({ ...info, [name]: Number(value) })   //---> yeni api ile birleştirmede işlem yapmama engel oldu. gelenin tür string olduğu için Number'ı kaldırıyorum
-    setInfo({ ...info, [name]: (value) })
+    setInfo({ ...info, [name]: value }) // sadece ID tutuluyor
   }
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
+    const requestData = {
+      ...info,
+      brand_id: info.brand_id,
+      product_id: info.product_id,
+      quantity: Number(info.quantity),
+      price: Number(info.price),
+    }
+
     if (info.id) {
-      putStockData("sales", info)
+      putStockData("sales", requestData)
     } else {
-      postStockData("sales", info)
+      postStockData("sales", requestData)
     }
 
     handleClose()
+    setInfo({})
   }
+
   return (
-    <div>
-      <Modal
-        open={open}
-        onClose={() => {
-          handleClose()
-        }}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={modalStyle}>
-          <Box
-            sx={{ display: "flex", flexDirection: "column", gap: 2 }}
-            component="form"
-            onSubmit={handleSubmit}
-          >
-            <FormControl>
-              <InputLabel variant="outlined" id="brand-select-label">
-                Brand
-              </InputLabel>
-              <Select
-                labelId="brand-select-label"
-                label="Brand"
-                id="brand-select"
-                name="brand_id"
-                value={info?.brand_id?._id || ""}
-                onChange={handleChange}
-                required
-              >
-                <MenuItem onClick={() => navigate("/stock/brands/")}>
-                  Add New Brand
-                </MenuItem>
-                <hr />
-                {brands?.map((item) => {
-                  return (
-                    <MenuItem key={item.id} value={item.id}>
-                      {item.name}
-                    </MenuItem>
-                  )
-                })}
-              </Select>
-            </FormControl>
-            <FormControl>
-              <InputLabel variant="outlined" id="product-select-label">
-                Product
-              </InputLabel>
-              <Select
-                labelId="product-select-label"
-                label="Product"
-                id="product-select"
-                name="product_id"
-                value={info?.product_id?._id || ""}
-                onChange={handleChange}
-                required
-              >
-                <MenuItem onClick={() => navigate("/stock/products")}>
-                  Add New Product
-                </MenuItem>
-                <hr />
-                {products?.map((item) => {
-                  return (
-                    <MenuItem key={item.id} value={item.id}>
-                      {item.name}
-                    </MenuItem>
-                  )
-                })}
-              </Select>
-            </FormControl>
-            <TextField
-              label="Quantity"
-              id="quantity"
-              name="quantity"
-              type="number"
-              variant="outlined"
-              InputProps={{ inputProps: { min: 0 } }}
-              value={info?.quantity || ""}
+    <Modal
+      open={open}
+      onClose={() => {
+        handleClose()
+        setInfo({})
+      }}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={modalStyle}>
+        <Box
+          sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+          component="form"
+          onSubmit={handleSubmit}
+        >
+          <FormControl>
+            <InputLabel id="brand-select-label">Brand</InputLabel>
+            <Select
+              labelId="brand-select-label"
+              label="Brand"
+              name="brand_id"
+              value={info.brand_id || ""}
               onChange={handleChange}
               required
-            />
-            <TextField
-              label="Price"
-              id="price"
-              type="number"
-              variant="outlined"
-              name="price"
-              InputProps={{ inputProps: { min: 0 } }}
-              value={info?.price || ""}
+            >
+              <MenuItem onClick={() => navigate("/stock/brands")}>
+                Add New Brand
+              </MenuItem>
+              <hr />
+              {brands?.map((item) => (
+                <MenuItem key={item.id} value={item.id}>
+                  {item.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          <FormControl>
+            <InputLabel id="product-select-label">Product</InputLabel>
+            <Select
+              labelId="product-select-label"
+              label="Product"
+              name="product_id"
+              value={info.product_id || ""}
               onChange={handleChange}
               required
-            />
-            <Button type="submit" variant="contained" size="large">
-              {info?.id ? "Update Sale" : "Add New Sale"}
-            </Button>
-          </Box>
-        </Box>
-      </Modal>
-    </div>
-  )
-}
+            >
+              <MenuItem onClick={() => navigate("/stock/products")}>
+                Add New Product
+              </MenuItem>
+              <hr />
+              {products?.map
